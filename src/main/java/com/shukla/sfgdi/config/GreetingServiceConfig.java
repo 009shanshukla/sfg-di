e9f46ed06b548ee0688814c7e683f52b2cf7f4ee
009shanshukla <shantanushukla61@gmail.com>
2023-05-28 16:13:32 +0530

    config class tutorial

---
+++ src/main/java/com/shukla/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,31 @@
+package com.shukla.sfgdi.config;
+
+import com.shukla.sfgdi.services.CatPetService;
+import com.shukla.sfgdi.services.DogPetService;
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * create by shuklash on 28/05/23
+ */
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
